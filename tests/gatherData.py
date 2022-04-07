3b2473e881dce75bab9c8e87e3e541a164ec42aa
Piotr Kakol <piotrek.krzysiek.kakol@gmail.com>
2022-04-07 09:45:27 +0200

    data from experiments

---
+++ tests/gatherData.py
@@ -0,0 +1,74 @@
+from fileinput import filename
+import json
+import os
+import scipy.stats as st 
+import numpy as np
+
+solvers = ["2Opt", "NN", "ExtNN", "2Opt-NN", "kRandom"]
+instanceSize = 100
+instance = "--euc2d {}".format(instanceSize)
+
+# data format: objective_function, iterations
+def gatherDataEntry(solver, additionalOptions = "", size = 100, k = 1000, start = 0, verbose=False):
+    if solver == "NN":
+        additionalOptions += (" --starting-node {}".format(start))
+    elif solver == "kRandom":
+        additionalOptions += (" --k {}".format(k))
+    if verbose:
+        call = "./build/Metaheurisitics --solver {} --size {} {} 2>/dev/null"\
+            .format(solver, size, additionalOptions)
+        print(call)
+    return [int(x) for x in os.popen("./build/Metaheurisitics --solver {} --size {} {} 2>/dev/null"\
+        .format(solver, size, additionalOptions)).read().split(" ")]
+
+
+def gatherData(fileName = "noLimits", iterations = 100, k = 100, solvers_=solvers, additionalOptions = ""):
+    res = {solver: {"dist": [], "iter": []} for solver in solvers_}
+    for solver in solvers_: 
+        for i in range(iterations):
+            entry = gatherDataEntry(solver, additionalOptions=instance +\
+                " --seed {}".format(i + 1) + additionalOptions, k=k)
+            res[solver]["dist"].append(entry[0])
+            res[solver]["iter"].append(entry[1])
+    if fileName != "":
+        with open(fileName, "w") as resFile:
+            resFile.write(json.dumps(res, indent=2))
+    return res
+
+def gatherDataLimitMin(fileName = "noLimits", iterations = 100, solvers_=solvers):
+    # res = gatherData("", iterations, solvers_=solvers_[:-2])
+    # minMedian = min([np.median(np.array([res[solver]["iter"]])) for solver in solvers_[:-2]])
+    return gatherData(fileName, iterations, instanceSize, solvers_, " --step-limit {}".format(instanceSize))
+
+
+def analize(solvers, data):
+    res = {}
+    for a in range(len(solvers)):
+        for b in range(a + 1, len(solvers)):
+            solA = solvers[a]
+            solB = solvers[b]
+            res["{} {}".format(solA, solB)] = [st.wilcoxon(data[solA]["dist"], data[solB]["dist"])]
+    return res
+
+
+def average(filePath, solvers):
+    with open(filePath, "r") as file:
+        data = json.loads(file.read())
+        for solver in solvers:
+            avg = sum(data[solver]["dist"]) / len(data[solver]["dist"])
+            print(solver + " avg distaace: {}".format(avg)) 
+
+
+if __name__ == "__main__":
+    iterations = 1
+    fileName = "iterations_{}_fast_problems_sizes_{}".format(iterations, instanceSize)
+    data = gatherData(fileName+".json", iterations, k=1000, solvers_=solvers)
+    # dataNoLimits = gatherDataLimitMin(fileName + "_no_limit.json", iterations, solvers)
+    # stats = analize(solvers, data)
+    # statsNoLimit = analize(solvers, dataNoLimits)
+    # with open(fileName + "_stats.json", "w") as file:
+    #     file.write(json.dumps(stats, indent=2))
+    # with open(fileName + "_stats_no_limit.json", "w") as file:
+    #     file.write(json.dumps(statsNoLimit, indent=2))
+    # average("iterations_100_problems_sizes_100.json", solvers)
+    
---
+++ tests/getDataFromTSP.py
@@ -0,0 +1,77 @@
+from distutils.log import debug
+import gatherData
+import os
+import json
+
+
+def getFilesList():
+    return os.popen("ls tests/problems/*.tsp tests/problems/*.atsp").read().split()
+
+def getSolutions(path):
+    with open(path, "r") as file:
+        return json.loads(file.read())
+
+def solveForFiles(files, solvers, resfile, iterations, k, optimalSolutions=None, limitByNN = False):
+    res = {file: {} for file in files}
+    res_errors = {file: {} for file in files}
+    res_avg = {solver: 0 for solver in solvers}
+
+    def dataEntry(solver, file, k = 1, start = 0, additionalOptions = ""):
+        return gatherData.gatherDataEntry(solver, "--from-file {}".\
+            format(file) + additionalOptions, k=k,start=start, verbose=False)
+    
+    if optimalSolutions != None:
+        for file in files:
+            res[file]["optimal"] = optimalSolutions[file.split(".")[0].split("/")[-1]]
+
+    for file in files:
+        addOpts = ""
+        if limitByNN:
+            addOpts = " --step-limit {}".format(dataEntry("NN", file)[1])
+        
+        for solver in solvers:
+            print("Solving: {} {}".format(solver, file))
+            if solver == "2Opt" or solver == "kRandom":
+                path = 0
+                for _ in range(iterations):
+                    path += dataEntry(solver, file, k, 0, addOpts)[0]
+                path /= iterations
+                res[file][solver] = path
+            else:
+                res[file][solver] = dataEntry(solver, file, k, 0, addOpts)[0]
+            if optimalSolutions != None:
+                optimalError =  (res[file]["optimal"] - res[file][solver])\
+                    / res[file]["optimal"]
+                res_errors[file][solver] = optimalError
+                res_avg[solver] += optimalError
+            
+    if optimalSolutions != None:
+        for solver in solvers:
+            res_avg[solver] /= len(files)
+        with open(resfile + "errors.json", "w") as file:
+            file.write(json.dumps(res_errors, indent=2))
+
+        with open(resfile + "erros_avg.json", "w") as file:
+            file.write(json.dumps(res_avg, indent=2))
+    
+    with open(resfile + ".json", "w") as file:
+        file.write(json.dumps(res, indent=2))
+    return res
+
+
+def solutionsToJSON(inFile, resFile):
+    with open(inFile) as file:
+        lines = file.readlines()
+        res = {}
+        for line in lines:
+            words = line.split()
+            res[words[0] if words[0][-1] != ":" else words[0][:-1]] = int(words[-1])
+        with open(resFile, "w") as jsonFile:
+            jsonFile.write(json.dumps(res, indent=2))
+
+if __name__ == "__main__":
+    files = getFilesList()
+    optimals = getSolutions("solutions.json")
+    res = solveForFiles(files, gatherData.solvers, "unlimited_solutions.json", 10, 1000, optimals)
+    # res = solveForFiles(files, gatherData.solvers, "limited_solutions.json", 10, 1000, optimals, True)
+    
